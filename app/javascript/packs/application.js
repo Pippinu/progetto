6c7e7de1ad7e221f1b38227548a7e2e7e3c5b60d
P1ppinu <iacono760@gmail.com>
2022-08-26 22:54:53 +0200

    return

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,27 @@
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+require("jquery")
+require("@popperjs/core")
+import "bootstrap"
+// import { Modal } from 'bootstrap'
+import { Tooltip, Popover } from 'bootstrap'
+
+jQuery(function () {
+  // alert(123456)
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new Tooltip(tooltipTriggerEl)
+  })
+
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new Popover(popoverTriggerEl)
+  })
+})
